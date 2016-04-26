a77b0bf91540a55444cbfebe870cb4f156177606
pchim <pchim24@gmail.com>
2016-04-26 10:16:31 -0700

    Add hover

--- src/clearDancer.js
+++ src/clearDancer.js
@@ -2,7 +2,17 @@
 var ClearDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('clear-dancer');
+
+  this.setHover();
 };
 
 ClearDancer.prototype = Object.create(makeDancer.prototype);
 ClearDancer.prototype.constructor = ClearDancer;
+ClearDancer.prototype.setHover = function() {
+  this.$node.hover((function() {
+    this.$node.css({opacity: 0.6});
+  }).bind(this),
+  (function() {
+    this.$node.css({opacity: 0.3});
+  }).bind(this));
+};
